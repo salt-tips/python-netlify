573b47eb054466d7faaa406dd9b13ea17297e122
Max Arnold <arnold.maxim@gmail.com>
2018-11-18 16:02:35 +0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='python-netlify',
+    version='0.0.1',
+    description='Python client for Netlify',
+    license='BSD',
+    url='http://github.com/salt-tips/python-netlify',
+    author='Max Arnold',
+    author_email='arnold.maxim@gmail.com',
+    packages=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet',
+    ],
+    platforms='any',
+)
